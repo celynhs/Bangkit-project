ef9c8561c420fbca7aa31835666ecc9666e044d0
AchSltnZer <97500965+AchSltnZer@users.noreply.github.com>
2023-06-12 17:11:15 +0700

    First

---
+++ app/src/main/java/com/sulton/belibijak/ui/auth/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.sulton.belibijak.ui.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sulton.belibijak.MainActivity
+import com.sulton.belibijak.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            super.onBackPressed()
+        }
+    }
+}
